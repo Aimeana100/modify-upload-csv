11b4522607ef1d16937100d1514084fe052c2c55
AnatholeK <anathole@amalitech.org>
2023-08-29 13:12:36 +0200

    modify handle upload function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Papa from 'papaparse';
+
 import './App.css';
 
 function App() {
+    const [csvData, setCsvData] = useState([]);
+    const [uploadStatus, setUploadStatus] = useState('');
+
+  const handleFileUpload = (e) => {
+    const file = e.target.files[0];
+  
+    Papa.parse(file, {
+      complete: (result) => {
+        // Add a new column 'Role' with a predefined value
+        const newData = result.data.map((row, index) => {
+          if (index === 0) {
+            // Add header for the new column
+            return [...row, 'Role'];
+          } else {
+            if (row[0]) {
+              // Determine role based on conditions (STUDENT or LECTURE)
+              const role = "STUDENT"; // or LECTURE accordingly
+              return [...row, role];
+            } else {
+              // If the row is empty, just return the row without modifying
+              return row;
+            }
+          }
+        });
+  
+        setCsvData(newData);
+      },
+    });
+  };
+
+  const handleDownload = () => {
+    const csv = Papa.unparse(csvData);
+    const blob = new Blob([csv], { type: 'text/csv' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'modified.csv';
+    a.click();
+  };
+
+  
+  const handleSubmit = async () => {
+    try {
+      const response = await fetch('/backend-endpoint', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(csvData),
+      });
+
+      if (response.ok) {
+        setUploadStatus('CSV data uploaded successfully.');
+      } else {
+        setUploadStatus('Failed to upload CSV data.');
+      }
+    } catch (error) {
+      console.error('Error uploading CSV data:', error);
+      setUploadStatus('An error occurred while uploading CSV data.');
+    }
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="file" accept=".csv" onChange={handleFileUpload} />
+      <button onClick={handleDownload}>Download Modified CSV</button>
+      <button onClick={handleSubmit}>Upload CSV Data</button>
     </div>
   );
 }
